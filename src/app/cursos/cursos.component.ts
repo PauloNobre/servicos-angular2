f2fd40769e09a32d8226b305546b510aefab9b2b
PauloNobre <paulinho140@gmail.com>
2019-05-03 09:11:49 -0300

    41: Comunicação entre componentes usando serviços (broadcast de eventos)

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -5,8 +5,8 @@ import { CursosService} from './cursos.service';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']//,
-  // providers: [CursosService]
+  styleUrls: ['./cursos.component.css'],
+  providers: [CursosService]
 })
 export class CursosComponent implements OnInit {
 
@@ -20,6 +20,12 @@ export class CursosComponent implements OnInit {
 
   ngOnInit() {
     this.cursos = this.cursosService.getCursos();
+    CursosService.criouNovoCurso.subscribe(
+      curso => {
+        console.log(curso);
+        this.cursos.push(curso);
+      }
+    );
   }
 
 }
